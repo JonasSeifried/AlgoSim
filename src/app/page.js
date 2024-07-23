0cb10a4780d91f94266592091442890773c43d73
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2024-07-23 21:52:53 +0200

    Add white outline. Fix z-index

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
 
 const RandomSim = dynamic(() => import('@/components/RandomSim'), { ssr: false });
 
 export default function Home() {
   return (
     <div className="relative flex flex-col items-center justify-center w-full h-full">
-      <div className="relative -top-[12.5%]">
+      <div className="relative -top-[12.5%] z-10 [text-shadow:_-1px_-1px_0_rgb(255_255_255),_1px_-1px_0_rgb(255_255_255),_-1px_1px_0_rgb(255_255_255),_1px_1px_0_rgb(255_255_255)]">
         <div className="m-4 text-center ">
           <h1 className="mb-1 text-4xl">Algo Sim</h1>
           <span>
@@ -14,7 +14,7 @@ export default function Home() {
         </div>
         <p>Some simulations I made for fun :)</p>
       </div>
-      <RandomSim className="absolute top-0 z-10 w-full h-full" />
+      <RandomSim className="absolute top-0 w-full h-full" />
     </div>
   );
 }
